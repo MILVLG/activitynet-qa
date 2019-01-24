445da127f2f82e25fa732a183d8ebc2d2881c95c
tena8808 <yuting8808@126.com>
2019-01-24 14:07:10 +0800

    val add

--- val/qa_analyze.py
+++ val/qa_analyze.py
@@ -35,14 +35,14 @@ def output(corrects, type_count):
 
     free_type_accuracy = free_type_corrects_count / float(sum(list(type_count.values())[3:]))
 
-    print('Motion: {}, Spat.Rel.: {}, Temp.Rel.: {}, Free: {}, All: {}'.format(accuracy[0], accuracy[1], accuracy[2], free_type_accuracy, all_type_accuracy))
-    print('Yes/No: {}, Color: {}, Object: {}, Location: {}, Number: {}, Other: {}'.format(accuracy[3], accuracy[4], accuracy[5], accuracy[6], accuracy[7], accuracy[8]))
+    print('Motion: {:.04f}, Spat.Rel.: {:.04f}, Temp.Rel.: {:.04f}, Free: {:.04f}, All: {:.04f}'.format(accuracy[0], accuracy[1], accuracy[2], free_type_accuracy, all_type_accuracy))
+    print('Yes/No: {:.04f}, Color: {:.04f}, Object: {:.04f}, Location: {:.04f}, Number: {:.04f}, Other: {:.04f}'.format(accuracy[3], accuracy[4], accuracy[5], accuracy[6], accuracy[7], accuracy[8]))
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pred_json', type=str, default='val/valpred.json',
+    parser.add_argument('--pred_json', type=str, default='/data1/yuting/esa_test1/valpred.json',
                         help='path to the json file containing your prediction')
-    parser.add_argument('--result_json', type=str, default='dataset/val_a.json',
+    parser.add_argument('--result_json', type=str, default='/data1/yuting/ActivityNetQA/val_a.json',
                         help='path to the json file containing the ground true')
     args = parser.parse_args()
     return args
